294bfbc299befb0fc403b28b73874bfcd5bb2772
miftahuljannahhh <mj5416386@gmail.com>
2025-06-02 15:21:02 +0700

    menambahkan final specifer pada baseclass

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -8,18 +8,4 @@ public:
     }
 };
 
-class derivedClass : public baseClass {
-    public:
-        void perkenalan() {
-            cout << "halo saya function dari derived Class";
-        }
-    };
 
-    int main() {
-        derivedClass a;
-        a.perkenalan();
-    
-        return 0;
-    }
-    
-    
