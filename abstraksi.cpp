7f3cc677bb4090be90b343addfbc90b5d067c032
miftahuljannahhh <mj5416386@gmail.com>
2025-06-02 15:11:50 +0700

    membuat fungsi prosedural yang bersifat public

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,4 +5,19 @@ using namespace std;
 class AbstraksiKlas {
     private: string x, y;
 
+    public:
+
+    //method untuk mengisi nilai
+    //private number
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+
+    }
 };
+
